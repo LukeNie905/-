0ee40a84a59cdb87bc94641444a83d218d088c02
LukeNie905 <157569877+LukeNie905@users.noreply.github.com>
2024-04-03 14:05:17 +0800

    roman

---
+++ roman/data.js
@@ -0,0 +1,305 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "羅馬競技場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.4173547436650722,
+        "pitch": -0.19220114768497965,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6254485274801134,
+          "pitch": 0.1315519202078228,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.0731332599588086,
+          "pitch": 0.20809903494238924,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6384479028286805,
+          "pitch": -0.3149407324083757,
+          "title": "羅馬競技場",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "羅馬競技場裡面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8988164985017519,
+          "pitch": 0.38934585863443516,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.6416505460170292,
+          "pitch": 0.045193480538268105,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0752079619935095,
+          "pitch": -0.0325331643863187,
+          "title": "羅馬競技場",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "萬神殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9775374242541846,
+          "pitch": 0.03742785932886861,
+          "rotation": 3.9269908169872414,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.7764835211420476,
+          "pitch": 0.08750282924838793,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0131361625212554,
+          "pitch": -0.17891743063474763,
+          "title": "萬神殿",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "萬神殿裡面",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10127630761939521,
+          "pitch": 0.29471581132048996,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.08598550821955087,
+          "pitch": -0.1564537202924452,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2174546928203842,
+          "pitch": 0,
+          "title": "萬神殿",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "人民廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6579945872621451,
+          "pitch": 0.041059981573418725,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.47401486136022797,
+          "pitch": 0.1581784148711698,
+          "rotation": 3.9269908169872414,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.081649407056588,
+          "pitch": -0.14970305625029745,
+          "title": "人民廣場",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "卡拉卡拉浴場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8389000233856319,
+          "pitch": -0.033923652156502726,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.8243917170542474,
+          "pitch": 0.3680001467274625,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2813714989771885,
+          "pitch": -0.16283270450276888,
+          "title": "卡拉卡拉育場",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "羅馬",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
